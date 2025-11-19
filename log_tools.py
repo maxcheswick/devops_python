93111a6447bde177f2d2284e01456b6ccbd38759
Max Cheswick <maxcheswick@gmail.com>
2025-11-18 22:04:22 -0500

    add modes for level and timestamp

--- log_tools.py
+++ log_tools.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 def read_logs(file_path):
     """
     Read all log files and return them as a list of strings
@@ -13,3 +16,56 @@ def read_logs(file_path):
         print(f"Error: Unexpected error while reading {file_path}: {e}")
         return []
             
+            
+def filter_by_level(file_path, level):
+    """
+    Return only the logs that are contained in that level.
+    Example levels: INFO, ERROR, WARNING, DEBUG, CRITICAL
+    """
+    level = level.upper()
+    lines = read_logs(file_path)
+    
+    filtered = []
+    for line in lines:
+        if level in line.upper():
+            filtered.append(line)
+        
+    return filtered
+
+
+def filter_by_time(file_path, start_time, end_time):
+    """
+    Filter the logs by a start time and an end time.
+    Expected timestamp format at the start of each log line:
+    YYYY-MM-DD HH:MM:SS
+
+    Example:
+    start_time = "2025-01-01 00:00:00"
+    end_time   = "2025-01-01 12:00:00"
+    """
+    
+    lines = read_logs(file_path)
+    
+    fmt = "%Y-%m-%d %H:%M:%S"
+    
+    start = datetime.datetime.strptime(start_time, fmt)
+    end =datetime.datetime.strptime(end_time, fmt)
+    
+    results = []
+    
+    for line in lines:
+        try:
+            # Expect the timestamp to be like:
+            # "2025-11-18 12:34:56 ERROR Something happened"
+            # So timestamp is the first two items when split by space
+            parts.line.split(" ")
+            timestamp = parts[0] + " " + parts[1]
+            ts = datetime.datetime.strptime(timestamp, fmt)
+            
+            if start <= ts <= end:
+                results.append(line)
+        except Exception:
+            # If no valid timestamp then skip it
+            continue
+    
+    return results
--- main.py
+++ main.py
@@ -2,17 +2,45 @@ import sys
 import log_tools
 
 def main():
-    if len(sys.argv) > 2:
-        print(f"Error: python main.py <log_file_path>")
+    if len(sys.argv) < 3:
+        print("Usage:")
+        print("  python main.py level <log_file> <LEVEL>")
+        print("  python main.py time  <log_file> <START> <END>")
         sys.exit(1)
+    
+    mode = sys.argv[1]
+    
+    if mode == "level":
+        if len(sys.argv) != 4:
+            print("Usage: python main.py level <log_file> <LEVEL>")
+            sys.exit(1)
+            
+        log_file = sys.argv[2]
+        level = sys.argv[3]
         
-    log_file = sys.argv[1]
-    lines = log_tools.read_logs(log_file)
-    
-    print(f"Read {len(lines)} from file {log_file}")
-    
-    for line in lines[:5]:
-        print(line.rstrip())
+        filtered = log_tools.filter_by_level(log_file, level)
+        # filtered = log_tools.read_logs(log_file)
+        
+        print(f"Read {len(filtered)} from file {log_file} with log level {level}")
+        
+        for line in filtered[:5]:
+            print(line.rstrip())
+    elif mode == "time":
+        if len(sys.argv) != 5:
+            print("  python main.py time  <log_file> <START> <END>")
+            sys.exit(1)
+        
+        log_file = sys.argv[2]
+        start = sys.argv[3]
+        end = sys.argv[4]
+        
+        filtered = log_tools.filter_by_time(log_file, start, end)
+        for line in filtered[:5]:
+            print(line.rstrip())
+    else:
+        print(f"Unknown mode: {mode}")
+        sys.exit(1)
+            
         
 if __name__ == "__main__":
     main()
